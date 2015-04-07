b50bd5ea718c9ec428687f299a5227a967c21196
jaredweiler <j_weiler1@u.pacific.edu>
2015-04-07 10:42:07 -0700

    added gauges

--- src/gl/gauge.java
+++ src/gl/gauge.java
@@ -33,17 +33,18 @@ import javax.swing.*;
 public class gauge extends javax.swing.JFrame {
     final static Radial gauge = new Radial();
     final static Radial gauge2 = new Radial();
+    final static Radial1Square gauge4 = new Radial1Square();
     final static LinearBargraph gauge3 = new LinearBargraph();
+    final static DigitalRadial gauge5 = new DigitalRadial();
     public static void createAndShowUI(final gaugeForm g) {
         final JFrame frame = new JFrame("Dashboard");
-        frame.setResizable(false);
-        Container con = frame.getContentPane();
-        con.setBackground(Color.black);
+        frame.setResizable(false);       
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //frame.setLocation(5, 510);
         frame.setLocationByPlatform(true);
         //frame.setBackground();
         //frame.setBackground(Color.black);
+        
         
         
         
@@ -66,8 +67,24 @@ public class gauge extends javax.swing.JFrame {
                 return new Dimension(100, 300);
             }
         };
+        JPanel panel4 = new JPanel() {
+            @Override 
+            public Dimension getPreferredSize() {
+                return new Dimension(300, 300);
+            }
+        };
+        JPanel panel5 = new JPanel() {
+            @Override 
+            public Dimension getPreferredSize() {
+                return new Dimension(300, 300);
+            }
+        };
         JPanel mainPanel= new JPanel();
+        JPanel mainPanel1=new JPanel();
+        JPanel mainPanel2=new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+        mainPanel1.setLayout(new BoxLayout(mainPanel1, BoxLayout.X_AXIS));
+        mainPanel2.setLayout(new BoxLayout(mainPanel2, BoxLayout.Y_AXIS));
 
         //final Radial gauge = new Radial();
         gauge.setTitle("MPH");
@@ -90,6 +107,27 @@ public class gauge extends javax.swing.JFrame {
         gauge3.setMinValue(-25);
         gauge3.setMaxValue(40);
         gauge.setLedVisible(false);
+        gauge4.setBackgroundColor(eu.hansolo.steelseries.tools.BackgroundColor.BLACK);
+        gauge4.setPointerColor(eu.hansolo.steelseries.tools.ColorDef.BLUE);
+        gauge4.setLcdColor(eu.hansolo.steelseries.tools.LcdColor.BLUEGRAY_LCD);
+        gauge4.setTitle("Fuel");
+        gauge4.setUnitString("Liters");
+        gauge4.setValue(60);
+        gauge4.setLedVisible(false);
+        gauge5.setLcdVisible(false);
+        gauge5.setTitle("Temperature");
+        gauge5.setUnitString("T");
+        gauge5.setBackgroundColor(eu.hansolo.steelseries.tools.BackgroundColor.BLACK);
+        gauge2.setTrackStart(8000);
+        gauge2.setTrackStop(10000);
+        gauge2.setTrackSectionColor(Color.GREEN);
+        gauge2.setTrackVisible(true);
+        
+        //gauge4.setTickmarkSectionsVisible(false);
+        //gauge4.setGaugeType(GaugeType.TYPE1);
+        //gauge4.setFrameDesign(FrameDesign.GOLD);
+        //gauge4.setFrameType(FrameType.SQUARE);
+        
         
         /* Track codes
         gauge2.setTrackStart(8000);
@@ -108,13 +146,23 @@ public class gauge extends javax.swing.JFrame {
         panel2.add(gauge2, BorderLayout.CENTER);
         panel3.setLayout(new BorderLayout());
         panel3.add(gauge3, BorderLayout.CENTER);
+        panel4.setLayout(new BorderLayout());
+        panel4.add(gauge4, BorderLayout.CENTER);
         panel.setBackground(Color.darkGray);
         panel2.setBackground(Color.darkGray);
         panel3.setBackground(Color.darkGray);
+        panel4.setBackground(Color.darkGray);
+        panel5.setLayout(new BorderLayout());
+        panel5.add(gauge5, BorderLayout.CENTER);
+        panel5.setBackground(Color.darkGray);
         mainPanel.add(panel);
         mainPanel.add(panel2);
         mainPanel.add(panel3);
-        frame.add(mainPanel);
+        mainPanel1.add(panel4);
+        mainPanel1.add(panel5);
+        mainPanel2.add(mainPanel);
+        mainPanel2.add(mainPanel1);
+        frame.add(mainPanel2);
 
         JPanel buttonsPanel = new JPanel();
         JLabel valueLabel = new JLabel("Value:");
@@ -144,7 +192,8 @@ public class gauge extends javax.swing.JFrame {
         //buttonsPanel.add(valueLabel);
         //buttonsPanel.add(valueField);
         //buttonsPanel.add(button);
-
+        g.pack();
+        g.setVisible(true);
         frame.add(buttonsPanel, BorderLayout.NORTH);
 
         frame.pack();
